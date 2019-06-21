e668bea60fc7773813213f96aec09495f09bc2db
Constantine <kostyaxxx8@gmail.com>
2019-06-21 23:07:36 +0300

    Create google auth button

--- app/src/main/java/ru/semper_viventem/confinder/ui/auth/AuthScreen.kt
+++ app/src/main/java/ru/semper_viventem/confinder/ui/auth/AuthScreen.kt
@@ -1,14 +1,26 @@
 package ru.semper_viventem.confinder.ui.auth
 
+import android.content.Intent
 import android.view.View
+import kotlinx.android.synthetic.main.screen_auth.*
 import ru.semper_viventem.confinder.R
 import ru.semper_viventem.confinder.ui.Screen
 
-class AuthScreen: Screen() {
+class AuthScreen : Screen() {
 
     override val layoutId: Int = R.layout.screen_auth
 
     override fun initView(view: View) {
-        // TODO init views
+        signIn.setOnClickListener { signIn() }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // TODO handle google auth result
+    }
+
+    private fun signIn() {
+        // TODO send google auth intent
     }
 }
