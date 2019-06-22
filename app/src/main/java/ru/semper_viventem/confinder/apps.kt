ad41c0b79ef158d748889c12a3fb42cf60255f58
Miha-x64 <silizium@ok.de>
2019-06-22 14:00:59 +0300

    fixed tg:// link

--- app/src/main/java/ru/semper_viventem/confinder/apps.kt
+++ app/src/main/java/ru/semper_viventem/confinder/apps.kt
@@ -16,6 +16,8 @@ object Telegram : App(
     R.drawable.ic_telegram_app
 ) {
     override fun open(activity: Activity, contact: String) {
-        activity.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("tg://$contact")))
+        activity.startActivity(Intent(Intent.ACTION_VIEW,
+            Uri.parse("tg://resolve").buildUpon().appendQueryParameter("domain", contact).build()
+        ))
     }
 }
