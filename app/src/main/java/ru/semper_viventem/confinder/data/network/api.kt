41c227adf33f408a7af1fa5de4d9588f7faa05ef
Miha-x64 <silizium@ok.de>
2019-06-22 06:49:59 +0300

    merge Api and api

--- app/src/main/java/ru/semper_viventem/confinder/data/network/api.kt
+++ app/src/main/java/ru/semper_viventem/confinder/data/network/api.kt
@@ -1,11 +1,36 @@
 package ru.semper_viventem.confinder.data.network
 
+import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.*
 import ru.semper_viventem.confinder.BuildConfig
+import ru.semper_viventem.confinder.data.Profile
+import ru.semper_viventem.confinder.data.ProfileCredentials
 
 val api: Api = Retrofit.Builder()
     .baseUrl(BuildConfig.API_URL)
     .addConverterFactory(GsonConverterFactory.create())
     .build()
     .create(Api::class.java)
+
+interface Api {
+
+    @GET("me")
+    fun getUser(@Header("API-KEY") authcode: String): Call<Profile>
+
+    @POST("me")
+    fun postUser(
+        @Header("API-KEY") authcode: String,
+        @Body profile: ProfileCredentials
+    ): Call<Profile>
+
+    @GET("swipe-list/{confId}")
+    fun getSwipeList(@Header("API-KEY") token: String, @Path("confId") confId: Int): Call<List<Profile>>
+
+    @POST("like/{userId}")
+    fun like(@Header("API-KEY") token: String, @Path("userId") userId: String): Call<Unit>
+
+    @POST("like/{userId}")
+    fun dislike(@Header("API-KEY") token: String, @Path("userId") userId: String): Call<Unit>
+}
